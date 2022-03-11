abbcf52f02bc26672d63cb81b5cee4dada9cbaab
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-03-11 18:32:59 +0100

    Sign up, log in, log out and DB user creation added

--- app.js
+++ app.js
@@ -1,4 +1,134 @@
+// *** FIREBASE SETUP ***:
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, deleteUser, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { getFirestore, collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCLUor0R1QCzEdIo0FEqH3VUOXWoitujDQ",
+    authDomain: "test-19cf1.firebaseapp.com",
+    projectId: "test-19cf1",
+    storageBucket: "test-19cf1.appspot.com",
+    messagingSenderId: "407722304456",
+    appId: "1:407722304456:web:1dfe1d12f2b60e9a3816fd"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+const nav = document.querySelector('nav');
+const title = document.querySelector('h1');
+const launchSection = document.querySelector('#launch-section');
+const signUpForm = document.querySelector('#sign-up-form');
+const logInForm = document.querySelector('#log-in-form');
+const displaySection = document.querySelector('#display-section');
+const toSignUpBtn = document.querySelector('#to-sign-up-btn');
+const toLogInBtn = document.querySelector('#to-log-in-btn');
+const myProfileBtn = document.querySelector('#my-profile-btn');
+const logOutBtn = document.querySelector('#log-out-btn');
+
+
+// *** FUNCTIONS ***
+// Sign Up function:
+const signUp = async (data) => {
+    if (data.signUpPassword === data.signUpPassword2 /* aquí los regex*/) {
+        try {
+            // Sign Up Process
+            await createUserWithEmailAndPassword(auth, data.signUpEmail, data.signUpPassword)
+                .then((userCredential) => {
+                    console.log('User registered');
+                })
+                .then(() => updateProfile(auth.currentUser, {
+                    displayName: data.signUpName
+                }))
+            //Create document in Firestore
+            await setDoc(doc(db, 'users', data.signUpEmail), {
+                userName: data.signUpName,
+                email: data.signUpEmail,
+                favourites: []
+            })
+        }
+        catch (error) {
+            console.log('Error: ', error)
+        }
+    }
+    else {
+        alert('Different passwords');
+    }
+}
+// Log In function:
+const logIn = async (data) => {
+    try {
+        await signInWithEmailAndPassword(auth, data.logInEmail, data.logInPassword)
+            .then(userCredential => {
+                const user = userCredential.user;
+            })
+    }
+    catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+        console.log(errorMessage)
+        alert('Incorrect user or password')
+    }
+}
+// Log Out function:
+const logOut = () => {
+    try {
+        auth.signOut();
+    }
+    catch(error) {
+        console.log(error);
+    }       
+}
+// Login observer function:
+const isUserLogged = () => {
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            console.log('Logged user: ' + user.displayName);
+        }
+        else {
+            console.log('No logged user');
+        }
+    })
+}
+//Function for fetching one books list:
+const getOneList = async (id) => {
+    try {
+        let request = await fetch(`https://api.nytimes.com/svc/books/v3/lists/${id}.json?api-key=aIIJ5p83TsAOWEXASdgbJYiNjZ1kSNW0`);
+        let response = await request.json();
+        return response.results.books
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+//Function for displaying one books list:
+const displayOneList = async (list) => {
+    list.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('book-card');
+        div.innerHTML = `<h2>#${book.rank} ${book.title}</h2>
+                         <p>${book.author}</p>
+                         <img src="${book.book_image}">
+                         <p>Weeks on list: ${book.weeks_on_list}</p>
+                         <p>${book.description}</p>
+                         <button type="button" class="buy-book-btn">Buy</button>`;
+        displaySection.appendChild(div);
+    })
+}
+
+// Wraper function for fetching and displaying the books list (it functions as an event listener)
+const getAndDisplayOneList = async (event) => {
+    const previousDivs = document.querySelectorAll('div');
+    [...previousDivs].forEach(div => div.remove());
+    // Parent element id of the button is the name of that list:
+    const id = event.target.parentElement.getAttribute('id');
+    const list = await getOneList(id);
+    await displayOneList(list);
+    createBackBtn();
+}
 
 // Function for fetching all lists:
 const getAllLists = async () => {
@@ -15,16 +145,19 @@ const getAllLists = async () => {
 //Function for displaying all lists:
 const displayAllLists = async (lists) => {
     lists.forEach(list => {
-        const main = document.querySelector('main');
         const div = document.createElement('div');
         div.classList.add('list-card');
         div.setAttribute('id', `${list.list_name_encoded}`)
         div.innerHTML = `<h2>${list.display_name}</h2>
                          <p>Oldest: ${list.oldest_published_date}</p>
-                         <p>Newest: ${list.newest}</p>
+                         <p>Newest: ${list.newest_published_date}</p>
                          <p>Updated: ${list.updated}</p>
-                         <button type="button" class="go-to-list-btn" onclick=goToList()>Go</button>`;
-        main.appendChild(div);
+                         <button type="button" class="go-to-list-btn">Go</button>`;
+        displaySection.appendChild(div);
+
+        // Set button event listeners:
+        const buttons = document.querySelectorAll('.go-to-list-btn');
+        [...buttons].forEach(button => button.addEventListener('click', getAndDisplayOneList))
     })
 }
 
@@ -43,41 +176,68 @@ const getAndDisplayAllLists = async () => {
         displayAllLists(lists);
     }
 }
-getAndDisplayAllLists();
 
-//Function for fetching one list
-const getOneList = async (id) => {
-    try {
-        let request = await fetch(`https://api.nytimes.com/svc/books/v3/lists/${id}.json?api-key=aIIJ5p83TsAOWEXASdgbJYiNjZ1kSNW0`);
-        let response = await request.json();
-        return response.results.books
-    }
-    catch (error) {
-        console.log(error)
-    }
+// Creating and displaying "Go Back" button:
+const createBackBtn = () => {
+    const btn = document.createElement('button');
+    btn.setAttribute('id', 'back-btn');
+    btn.innerText = 'Go Back';
+    displaySection.insertBefore(btn, displaySection.firstChild);
+    btn.addEventListener('click', () => {
+        const previousDivs = document.querySelectorAll('div');
+        [...previousDivs].forEach(div => div.remove());
+        getAndDisplayAllLists();
+        btn.remove();
+    });
 }
 
-//Function for displaying one list
-const displayOneList = async (list) => {
-    list.forEach(book => {
-        const main = document.querySelector('main');
-        const div = document.createElement('div');
-        div.classList.add('book-card');
-        div.innerHTML = `<h2>#${book.rank} ${book.title}</h2>
-                         <p>${book.author}</p>
-                         <img src="${book.book_image}">
-                         <p>Weeks on list: ${book.weeks_on_list}</p>
-                         <p>${book.description}</p>
-                         <button type="button" class="buy-book-btn">Buy</button>`;
-        main.appendChild(div);
-    })
-}
+// Launch page button events:
+toSignUpBtn.addEventListener('click', () => {
+    signUpForm.classList.toggle('scaled');
+});
+toLogInBtn.addEventListener('click', () => {
+    logInForm.classList.toggle('scaled');
+});
 
-//Button event going to clicked list (wrapper function for asynchrony)
-const goToList = async () => {
-    let previousDivs = document.querySelectorAll('div');
-    [...previousDivs].map(div => div.remove());
-    const id = window.event.target.parentElement.getAttribute('id');
-    const list = await getOneList(id);
-    await displayOneList(list);
-}
+// Sign up and Log in button events:
+signUpForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    launchSection.classList.toggle('off');
+    signUpForm.classList.toggle('scaled');
+    displaySection.classList.toggle('off');
+    nav.classList.toggle('off');
+    const data = {
+        signUpName: event.target['sign-up-name'].value,
+        signUpEmail: event.target['sign-up-email'].value,
+        signUpPassword: event.target['sign-up-password'].value,
+        signUpPassword2: event.target['sign-up-password2'].value,
+    };
+    await signUp(data);
+    await getAndDisplayAllLists();
+});
+
+logInForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    launchSection.classList.toggle('off');
+    logInForm.classList.toggle('scaled');
+    displaySection.classList.toggle('off');
+    nav.classList.toggle('off');
+    const data = {
+        logInEmail: event.target['log-in-email'].value,
+        logInPassword: event.target['log-in-password'].value,
+    };
+    await logIn(data);
+    await getAndDisplayAllLists();
+});
+
+// Log Out button event:
+logOutBtn.addEventListener('click', () => {
+    logOut();
+    displaySection.classList.toggle('off');
+    launchSection.classList.toggle('off');
+    nav.classList.toggle('off');
+});
+
+isUserLogged();
